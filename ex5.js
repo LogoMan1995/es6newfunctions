ddef6ca57c56b4da54e7954a5b32fca5b346ebd1
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-02 18:23:16 +0400

    Write Arrow Functions with Parameters

---
+++ ex5.js
@@ -0,0 +1,7 @@
+// 5. Перепишите myConcat функцию, которая добавляет содержимое в arr2, arr1 чтобы она использовала синтаксис функции стрелки.
+
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+  };
+ 
+  console.log(myConcat([1, 2], [3, 4, 5]));
