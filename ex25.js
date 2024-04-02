d91c1358ced8f87e4efd14d49074d4b37284754e
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-02 23:13:33 +0400

    Create a JavaScript Promise

---
+++ ex25.js
@@ -0,0 +1,8 @@
+// 25. Создайте новое обещание под названием makeServerRequest. Передайте функцию с параметрами resolve и reject в конструктор.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
+
+
+
