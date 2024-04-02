8201c706e4e7ba4e275fcff05f3b7b50dc250ddd
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-02 22:56:20 +0400

    Use * to Import Everything from a File

---
+++ ex22.js
@@ -0,0 +1,9 @@
+// 22. Код в этом файле требует содержимого файла: string_functions.js, который находится в том же каталоге, что и текущий файл. Используйте import * as синтаксис, чтобы импортировать все из файла в объект с именем stringFunctions.
+
+
+
+import * as stringFunctions from "./string_functions.js";
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
