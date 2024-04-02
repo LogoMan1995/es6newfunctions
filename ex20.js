fc0d4d7140dc333bc66f4de26c08a6cf33abec8f
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-02 22:49:08 +0400

    Use export to Share a Code Block

---
+++ ex20.js
@@ -0,0 +1,8 @@
+// 20. В редакторе есть две функции, связанные со строками. Экспортируйте их оба, используя метод по вашему выбору.
+
+export const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+export const lowercaseString = (string) => {
+    return string.toLowerCase();
+  }
