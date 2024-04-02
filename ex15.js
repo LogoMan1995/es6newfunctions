7cd20249ac2675c96dcec3710b31b395f7413b11
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-02 20:26:38 +0400

    Write Concise Object Literal Declarations Using Object Property Shorthand

---
+++ ex15.js
@@ -0,0 +1,12 @@
+// 15. Используйте сокращение свойств объекта с литералами объекта, чтобы создать и вернуть объект со свойствами и name, age, gender.
+
+
+const createPerson = (name, age, gender) => {
+    return {
+      name,
+      age,
+      gender
+    };
+  };
+
+
